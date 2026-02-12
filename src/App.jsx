83af31fceef5dbc1424c6a4c03dacb18064ac376
cp985 <166384434+cp985@users.noreply.github.com>
2026-02-12 14:56:48 +0100

    working on navbar and theme

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RouterProvider  router={router} />
     </QueryClientProvider>
   );
 }
--- src/components/UI/MainNav.jsx
+++ src/components/UI/MainNav.jsx
@@ -1,33 +1,53 @@
 //! nav fissa una volta dentro. pensare se mettere
 //! filti qui  o creare barra laterale
+import { useState,useEffect } from "react";
+
 import classMainNav from "./style/MainNav.module.css";
 import Button from "./Button";
-export default function MainNav() {
-  return (
-      <nav className={classMainNav.nav} >
-        <ul className={classMainNav.ul}>
-                   <li>
-            <Button text={"Home"} isLink={true} path={"home"} />
-          </li>
-          <li>
-            <Button text={"Shop"} isLink={true} path={"shop"} />
-          </li>
-          <li>
-            <Button text={"Account"} isLink={true} path={"accountUser/:id"} />
-          </li>
 
-          <li>
-            <Button
-              text={"NewsLetterContact"}
-              isLink={true}
-              path={"newsLetterContact"}
-            />
-          </li>
-          <li>
-            <Button text={`Cart(${0})`} isLink={true} path={"cart"} />
-          </li>
-        </ul>
-      </nav>
-  
+
+export default function MainNav() {
+const [theme,setTheme]=useState('light')
+
+function toggleTheme() {
+  setTheme(theme==='dark' ? 'light' : 'dark')
+}
+
+useEffect(() => {
+  document.body.setAttribute('data-theme', theme);}, [theme]);
+
+  return (
+    <nav className={classMainNav.nav}>
+      <ul className={classMainNav.ul}>
+        <li>
+          <Button text={"Home"} isLink={true} path={"home"} />
+        </li>
+        <li>
+          <Button text={"Shop"} isLink={true} path={"shop"} />
+        </li>
+        <li className={classMainNav.li}>
+          <Button text={"Account"} isLink={true} path={"accountUser/:id"} />
+        <div>
+        <Button text={"Orders"} isLink={true} path={"accountUser/:id/orders"} />
+        <Button text={"Profile"} isLink={true} path={"accountUser/:id/profile"} />
+        </div>
+        
+        </li>
+
+        <li>
+          <Button
+            text={"NewsLetterContact"}
+            isLink={true}
+            path={"newsLetterContact"}
+          />
+        </li>
+        <li>
+          <Button text={`Cart(${0})`} isLink={true} path={"cart"} />
+        </li>
+        <li>
+          <Button classOf={'secondaryButton'} text={theme ==='dark' ? 'Light' : 'Dark'}  onClick={toggleTheme} />
+        </li>
+      </ul>
+    </nav>
   );
 }
